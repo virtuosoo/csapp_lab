5c99b086fa368eb6a380add5a660a226b247e316
azusa jixiegeming <w572400957@163.com>
2023-11-29 22:15:20 +0800

    update

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "virtuoso",
     /* First member's full name */
-    "Harry Bovik",
+    "Wang Zi",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "w572400957@163.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
