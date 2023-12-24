b48bb27edbbbcf7241f8d6b611ace5c4e7ce744b
azusa jixiegeming <w572400957@163.com>
2023-12-24 21:16:47 +0800

    update

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
+#include <csapp.h>
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000  //~= 1MB
 #define MAX_OBJECT_SIZE 102400  //100 KB
 
+
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static char * proxyPort = "8082";
+
 
 int main()
 {
-    printf("%s", user_agent_hdr);
+    int listenfd, connfd;
+    
     return 0;
 }
