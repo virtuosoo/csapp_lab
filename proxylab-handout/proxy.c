8eed7108b8cac7a9c6872d1bf95e0260c659cf7b
azusa jixiegeming <w572400957@163.com>
2023-12-27 00:14:06 +0800

    update

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -12,10 +12,8 @@
 #define HOST_LEN 128
 #define HTTP_VERSION_LEN 16
 
-/* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
-static const char *conn_hdr = "Connection: close\r\n";
-static const char *proxy_conn_hdr = "Proxy-Connection: close\r\n";
+#define ARRAY_LEN(arr) sizeof(arr) / sizeof((arr)[0])
+
 static const char *httpPrefix = "http://";
 static const int httpPrefixLen = 7;
 
@@ -24,17 +22,19 @@ static char *proxyPort = "8082";
 static char *version = "HTTP/1.0";
 
 
-char *supplied_hdrs[] = {"User-Agent", "Connection", "Proxy-Connection", "Host"};
+char *supplied_hdrs[] = {"User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n", 
+                        "Connection: close\r\n", 
+                        "Proxy-Connection: close\r\n"};
 
 void *work(void *connfd);
 int parseRequestLine(char *requstLine, char *method, char *host, char **uri);
 int connectToHost(char *host);
 int rio_readlineb_limit(rio_t *rp, void *usrbuf, size_t maxlen);
+int forwardRequestHdrs(rio_t *rioClient, rio_t *rioServer);
 
 int main()
 {
-    int listenfd, connfd;
-    char host[HOSTNAME_LEN], port[PORT_LEN];
+    int listenfd, *connfd;
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
     listenfd = Open_listenfd(proxyPort);
@@ -42,7 +42,7 @@ int main()
     while (1) {
         pthread_t tid;
         clientlen = sizeof(clientaddr);
-        int *connfd = (int *) Malloc(sizeof(int));
+        connfd = (int *) Malloc(sizeof(int));
         *connfd = accept(listenfd, (SA*) &clientaddr, &clientlen);
         if (*connfd < 0) {
             printf("accept failed, %s\n", strerror(errno));
@@ -55,17 +55,17 @@ int main()
 
 void* work(void *fd)
 {
-    int connfd = *((int *) fd), rc;
+    int clientfd = *((int *) fd), rc;  //fd that communicate with client
     Free(fd);
     pthread_detach(pthread_self());
     char method[METHOD_LEN], host[HOST_LEN];
-    char buf[MAXLINE];
+    char buf[MAXLINE], sendBuf[MAXLINE];
 
-    rio_t rio;
-    rio_readinitb(&rio, connfd);
-    rc = rio_readlineb_limit(&rio, buf, MAXLINE); //read requst line
+    rio_t rioClient, rioServer;
+    rio_readinitb(&rioClient, clientfd);
+    rc = rio_readlineb_limit(&rioClient, buf, MAXLINE); //read requst line
     if (rc < 0) {
-        close(connfd);
+        close(clientfd);
         return NULL;
     }
 
@@ -76,9 +76,32 @@ void* work(void *fd)
     char *uri;
     if (parseRequestLine(buf, method, host, &uri) < 0) {
         printf("parse request line failed, %s\n", buf);
+        return NULL;
     }
 
-    int clientfd = connectToHost(host);
+    int serverfd = connectToHost(host); //fd that communicate with server
+    if (serverfd < 0) {
+        printf("connect to server failed\n");
+        return NULL;
+    }
+
+    rio_readinitb(&rioServer, serverfd);
+
+    sprintf(sendBuf, "%s %s %s\r\n", method, uri, version);
+    if (rio_writen(serverfd, sendBuf, strlen(sendBuf)) < 0) {
+        printf("send requst line failed, error(%s)\n", strerror(errno));
+        return NULL;
+    }
+    sprintf(sendBuf, "Host: %s\r\n", host);
+    if (rio_writen(serverfd, sendBuf, strlen(sendBuf)) < 0) {
+        printf("send Host header failed, error(%s)\n", strerror(errno));
+        return NULL;
+    }
+
+    if (forwardRequestHdrs(&rioClient, &rioServer) < 0) {
+        printf("send request headers failed, error(%s)\n", strerror(errno));
+        return NULL;
+    }
 
 }
 
@@ -92,7 +115,7 @@ int rio_readlineb_limit(rio_t *rp, void *usrbuf, size_t maxlen)
     }
 
     if (((char *)usrbuf)[rc - 1] != '\n') { //line is too long
-        printf("line is too long, %s\n", usrbuf);
+        printf("line is too long, %s\n", (char *)usrbuf);
         return -1;
     }
     return rc;
@@ -134,8 +157,10 @@ int parseRequestLine(char *requstLine, char *method, char *host, char **uri)
     return 0;
 }
 
-int connectToHost(char *host)
+int connectToHost(char *s)
 {
+    char host[HOST_LEN];
+    strcpy(host, s);
     char *domainName = host, *port;
     char *c = strchr(host, ':');
     if (c == NULL) {
@@ -145,5 +170,43 @@ int connectToHost(char *host)
         port = c + 1;
     }
 
+    return open_clientfd(domainName, port);
+}
 
+int forwardRequestHdrs(rio_t *rioClient, rio_t *rioServer)
+{
+    char buf[MAXLINE];
+    while (1) {
+        int rc;
+        if ((rc = rio_readlineb_limit(rioClient, buf, MAXLINE)) < 0) {
+            return -1;
+        }
+
+        if (strncmp(buf, "Host", 4) == 0) { //we have sent Host header before, ignore here
+            continue;
+        }
+        
+        int i;
+        for (i = 0; i < ARRAY_LEN(supplied_hdrs); ++i) {
+            char *p = strchr(supplied_hdrs[i], ':');
+            int len = p - supplied_hdrs[i];
+            if (strncmp(buf, supplied_hdrs[i], len) == 0) {
+                break;
+            }
+        }
+
+        int wc;
+        if (i == ARRAY_LEN(supplied_hdrs)) { //send the original header
+            wc = rio_writen(rioServer->rio_fd, buf, rc);
+        } else {
+            wc = rio_writen(rioServer->rio_fd, supplied_hdrs[i], strlen(supplied_hdrs[i]));
+        }
+
+        if (wc < 0) {
+            return -1;
+        }
+
+        if (strcmp(buf, "\r\n") == 0) break;
+    }
+    return 1;
 }
